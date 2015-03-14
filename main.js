34d49c7bffb9e9dd4dda4e5fd3543a040ee61dcb
Ben Haaland <bphaaland@gmail.com>
2015-03-14 15:28:28 -0400

    adding some sheezy to the notes, and made a click event application

--- main.js
+++ main.js
@@ -1,7 +1,40 @@
 /**
  * Created by bphaaland on 3/14/15.
  */
-console.log('test test');
+//console.log('test test');
+
+//var outerEl = document.getElementById('outer');
+//var innerEl = document.getElementById('inner');
+
+
+function queryId(id) {
+    return document.getElementById(id);
+}
+
+var query = {
+    id: queryId
+};
+
+var outerEl = query.id('outer');
+var innerEl = query.id('inner');
+
+function alertClicked(event) {
+    console.log(event);
+    console.log('the red shit is clicked');
+}
+
+outerEl.addEventListener('click', alertClicked)
+
+
+
+
+
+
+
+
+
+
+
 
 
 
